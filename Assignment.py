e9c3d2f93da97b8f8653249a430c3053d028bc5b
beproject2020 <55380863+beproject2020@users.noreply.github.com>
2021-03-23 11:43:20 +0530

    App run testing Completed

---
+++ Assignment.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import random
+import string
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "Hello world"
+
+@app.route('/assignment')
+def hello_assign():
+    return "Assignment"
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
