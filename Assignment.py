ea18cc2d558c6a876aa4c64b6bcdb27d6a396fc3
beproject2020 <55380863+beproject2020@users.noreply.github.com>
2021-03-23 13:21:59 +0530

    write in url file check string in file shorten url found

--- Assignment.py
+++ Assignment.py
@@ -6,11 +6,44 @@ import os
 
 app = Flask(__name__)
 
+
+def write_in_url_file(longurl,shorturl):
+    urlfile = open("urlFile.text",'w')
+    stored_url = str(longurl)+":"+str(shorturl)+"\n"
+    urlfile.write(str(stored_url))
+    urlfile.close() 
+
+def check_if_string_in_file(file_name, string_to_search):
+  
+   
+    with open(file_name, 'r') as read_obj:
+        
+        for line in read_obj:
+      
+            if string_to_search in line:
+                return True
+    return False
+
+def shorten_url():
+    letters = string.ascii_lowercase + string.ascii_uppercase
+    while True:
+        rand_letters = random.choices(letters,k=3)
+        rand_letters = "".join(rand_letters)
+        shorten_url = check_if_string_in_file("urlFile.text",str(rand_letters))
+        if not shorten_url:
+            return rand_letters
+
  
 @app.route('/',methods=['POST','GET'] )
 def home():
     if request.method == 'POST':
         url_recevied = request.form["nm"]
+        found_url = check_if_string_in_file("urlFile.text",str(url_recevied))
+        if found_url:
+            return "short url"
+        else:
+            short_url = shorten_url()
+            write_in_url_file( url_recevied,short_url) 
         return url_recevied
     else:
         return render_template("home.html")
